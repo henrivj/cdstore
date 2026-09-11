c964fe03de122ee4c139d2f29ebaa4ccfcd13178
henrivj <henrique_justus@estudante.sesisenai.org.br>
2026-09-11 18:10:52 -0300

    feat: responsive layout

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function setMainMargin() {
-	const header = document.querySelector("header")
-	const main = document.querySelector("main")
-
-	main.style.marginTop = `${header.offsetHeight * 3}px`
-}
-
-setMainMargin()
